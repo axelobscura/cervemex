a7efdd0c5f77bf74f93a6c3fd512001a333eca9b
axelobscura <axosar@gmail.com>
2020-09-29 14:11:26 -0500

    Carousel entrada

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import Layout from '../components/Layout'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import CarouselIntro from '../components/CarouselIntro'
 
 export default function Home() {
   return (
-    <Layout>
-      <Row>
-        <Col>
-          <h1>
-            Bienvenido a <a href="">CERVEMEX!</a>
-          </h1>
+    <Layout fluid>
+      <Row style={{margin: '0', padding: '0'}}>
+        <Col style={{margin: '0', padding: '0'}}>
+          <CarouselIntro />
         </Col>
       </Row>
     </Layout>
