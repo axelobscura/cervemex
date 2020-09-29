28d9550151e7f18f1807e83d3d068fb8d875fedd
axelobscura <axosar@gmail.com>
2020-09-29 14:32:26 -0500

    Colocar tipografia

--- components/CarouselIntro.js
+++ components/CarouselIntro.js
@@ -6,9 +6,11 @@ export default function CarouselIntro() {
         <Carousel>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 h-100"
                     src="https://images.pexels.com/photos/159291/beer-machine-alcohol-brewery-159291.jpeg"
-                    alt="First slide"
+                    alt="Catálogo de la cerveza mexicana"
+                    title="Catálogo de la cerveza mexicana"
+                    style={{ height: '100vh !important' }}
                 />
                 <Carousel.Caption>
                     <h3>CERVEZA MEXICANA</h3>
@@ -17,9 +19,11 @@ export default function CarouselIntro() {
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 h-100"
                     src="https://images.pexels.com/photos/1796698/pexels-photo-1796698.jpeg"
-                    alt="Third slide"
+                    alt="Catálogo de la cerveza mexicana"
+                    title="Catálogo de la cerveza mexicana"
+                    style={{ height: '100vh !important' }}
                 />
 
                 <Carousel.Caption>
@@ -29,9 +33,11 @@ export default function CarouselIntro() {
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 h-100"
                     src="https://images.pexels.com/photos/250465/pexels-photo-250465.jpeg"
-                    alt="Third slide"
+                    alt="Catálogo de la cerveza mexicana"
+                    title="Catálogo de la cerveza mexicana"
+                    style={{ height: '100vh !important' }}
                 />
 
                 <Carousel.Caption>
