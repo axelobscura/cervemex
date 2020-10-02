18c39ac2cf635b491d6af96088c092da13ddebaf
axelobscura <axosar@gmail.com>
2020-10-01 20:09:49 -0500

    Agregando favicon

--- components/Layout.js
+++ components/Layout.js
@@ -10,8 +10,8 @@ export default function Layout({ children }){
         <Container fluid={true}>
             <Head>
                 <title>{siteTitle}</title>
-                <link rel="icon" type="image/png" href="/img/logo.png" />
-                <meta name="description" content="Learn how to build a personal website using Next.js" />
+                <link rel="icon" type="image/png" href="favicon.png" />
+                <meta name="description" content="Catálogo de la cerveza mexicana, encuentre de manera sencilla las mejores mracas de cerveza en México." />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
             </Head>
