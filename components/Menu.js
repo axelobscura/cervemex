d2e246c1dfa2ab8950d1b5c5aa49247449850f40
axelobscura <axosar@gmail.com>
2020-10-01 20:49:00 -0500

    secciones

--- components/Menu.js
+++ components/Menu.js
@@ -36,15 +36,16 @@ export default function Menu() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#home">CATÁLOGO</Nav.Link>
+            <Nav.Link href="#link">TIENDA</Nav.Link>
+            <NavDropdown title="TIPOS" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">CATÁLOGO</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#link">CONTACTO</Nav.Link>
             </Nav>
             <Form inline>
             <input type="text" placeholder="Buscar cerveza..." className="mr-sm-2" style={elInput} />
